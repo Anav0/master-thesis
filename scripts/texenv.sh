7cd8abfb1e1402aae31b4b4ee595c8fdd97c3ad0
Igor Motyka <32037658+Anav0@users.noreply.github.com>
2021-03-05 16:58:59 +0100

    Initial commit

---
+++ scripts/texenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Determine platform environment (texlive or miktex)
+if [[ $OSTYPE == "linux-gnu" ]] ; then
+    TEXENV="texlive"
+elif [[ $OSTYPE == "msys" ]] ; then
+    if [[ $(which pdftex) =~ miktex ]] ; then
+        TEXENV="miktex"
+    elif [[ $(which pdftex) =~ texlive ]] ; then
+        TEXENV="texlive"
+    fi
+fi
+
+echo "$TEXENV"
